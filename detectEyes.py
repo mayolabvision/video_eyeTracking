59b35cf50c65b6df2a163d0c4feda9b4c504f615
kendranoneman <knoneman@andrew.cmu.edu>
2024-02-16 17:23:17 -0500

    removing big file

--- detectEyes.py
+++ detectEyes.py
@@ -4,12 +4,12 @@ from operator import rshift
 import mediapipe as mp
 import dlib
 
-from google.colab import files
-from google.colab.patches import cv2_imshow
 import os
 import subprocess
 from tqdm import tqdm
 
-from helpers import convert_mov_to_flv 
+from helpers import convert_mov_to_flv,get_landmarksFile
+
+get_landmarksFile()
 
 landmarksFile =  "/content/shape_predictor_68_face_landmarks.dat"
